ff9092d1f7030c184e7311a97bcbfa054bff8ec1
Jeremy Moles <cubicool@gmail.com>
2017-02-08 14:05:08 -0500

    Adds a "scratchpad" seq-tmp testing executable

--- test/seq-test.c
+++ test/seq-test.c
@@ -1,9 +1,10 @@
-#include "test.h"
+#include "seq-test.h"
 
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 typedef struct _test_t {
 	int i;
@@ -33,6 +34,7 @@ static test_t* test_create(int i, float f, double d, const char* s) {
 	test_printf("}");
 } */
 
+const seq_size_t test_strings_size = 12;
 const char* test_strings[] = {
 	"foo", "Foo", "FOO",
 	"bar", "Bar", "BAR",
@@ -40,15 +42,12 @@ const char* test_strings[] = {
 	"qux", "Qux", "QUX"
 };
 
-const seq_size_t test_strings_size = 12;
-
 void test_strings_append(seq_t seq) {
 	seq_size_t i;
 
 	for(i = 0; i < test_strings_size; i++) seq_add(seq, SEQ_APPEND, test_strings[i]);
 }
 
-/* ============================================================================================= */
 SEQ_TEST_BEGIN(add_append_prepend)
 	SEQ_ASSERT( seq_add(seq, SEQ_APPEND, "bar") )
 	SEQ_ASSERT( seq_add(seq, SEQ_APPEND, "baz") )
@@ -108,7 +107,6 @@ SEQ_TEST_BEGIN(add_replace)
 	SEQ_ASSERT_STRCMP( seq_get(seq, SEQ_INDEX, 2), "baz" )
 SEQ_TEST_END
 
-/* ============================================================================================= */
 SEQ_TEST_BEGIN(remove_free)
 	test_t* t = test_create(1, 2.2f, 33.33, "FOUR");
 
@@ -118,7 +116,6 @@ SEQ_TEST_BEGIN(remove_free)
 	SEQ_ASSERT( seq_size(seq) == 0)
 SEQ_TEST_END
 
-/* ============================================================================================= */
 seq_data_t on_add(seq_args_t args) {
 	unsigned long foo = seq_arg(args, unsigned long);
 	unsigned long bar = seq_arg(args, unsigned long);
@@ -140,7 +137,6 @@ SEQ_TEST_BEGIN(on_add_remove)
 	SEQ_ASSERT( seq_remove(seq, SEQ_INDEX, 0) )
 SEQ_TEST_END
 
-/* ============================================================================================= */
 seq_data_t on_add_addr(seq_args_t args) {
 	return seq_arg_data(args);
 }
@@ -164,7 +160,6 @@ SEQ_TEST_BEGIN(on_add_remove_addr)
 	SEQ_ASSERT( t == NULL )
 SEQ_TEST_END
 
-/* ============================================================================================= */
 SEQ_TEST_BEGIN(add_errors)
 	SEQ_ASSERT( seq_add(seq, SEQ_APPEND, "foo") )
 	SEQ_ASSERT( !seq_add(seq, "bar") )
@@ -175,7 +170,6 @@ SEQ_TEST_BEGIN(add_errors)
 	SEQ_ASSERT( !seq_remove(seq, SEQ_INDEX, -3) )
 SEQ_TEST_END
 
-/* ============================================================================================= */
 SEQ_TEST_BEGIN(iterate)
 	test_strings_append(seq);
 
@@ -201,17 +195,81 @@ SEQ_TEST_BEGIN(iterate)
 	}
 SEQ_TEST_END
 
-/* ============================================================================================= */
+typedef struct _test_func_t {
+	void (*func)(const char*);
+	const char* name;
+	const char* descr;
+} test_func_t;
+
+#define TEST_FUNC(name, descr) { test_##name, #name, #descr }
+
+const seq_size_t test_funcs_size = 9;
+const test_func_t test_funcs[] = {
+	TEST_FUNC(add_append_prepend, "SEE_APPEND / SEQ_PREPEND"),
+	TEST_FUNC(add_before, "SEQ_BEFORE"),
+	TEST_FUNC(add_after, "SEQ_AFTER"),
+	TEST_FUNC(add_replace, "SEQ_REPLACE"),
+	TEST_FUNC(remove_free, "SEQ_REMOVE_FREE"),
+	TEST_FUNC(on_add_remove, "SEQ_CB_ADD / SEQ_CB_REMOVE"),
+	TEST_FUNC(on_add_remove_addr, "SEQ_CB_ADD / SEQ_CB_REMOVE"),
+	TEST_FUNC(add_errors, "SEQ_ADD (ERRORS)"),
+	TEST_FUNC(iterate, "SEQ_INC / SEQ_RANGE"),
+};
+
+static seq_bool_t run_tests(const char* str, seq_size_t index, seq_bool_t all) {
+	seq_bool_t any = SEQ_FALSE;
+	seq_size_t i;
+
+	for(i = 0; i < test_funcs_size; i++) {
+		const test_func_t* tf = &test_funcs[i];
+
+		if(all || i == index || (str && strstr(tf->name, str))) {
+			tf->func(tf->descr);
+
+			any = SEQ_TRUE;
+		}
+	}
+
+	return any;
+}
+
 int main(int argc, char** argv) {
-	test_add_append_prepend("SEQ_APPEND / SEQ_PREPEND");
-	test_add_before("SEQ_BEFORE");
-	test_add_after("SEQ_AFTER");
-	test_add_replace("SEQ_REPLACE");
-	test_remove_free("SEQ_REMOVE_FREE");
-	test_on_add_remove("SEQ_CB_ADD / SEQ_CB_REMOVE");
-	test_on_add_remove_addr("SEQ_CB_ADD / SEQ_CB_REMOVE");
-	test_add_errors("SEQ_ADD (ERRORS)");
-	test_iterate("SEQ_INC / SEQ_RANGE");
+	if(argc == 1) {
+		int t;
+
+		printf(
+			"Specify one or more test names to run, separated by\n"
+			"spaces. Using substrings is permitted, as well as the\n"
+			"special keyword 'all' (which runs every test).\n"
+			"Alternatively, you can use the numeric index indicated\n"
+			"in the list below.\n"
+			"\n"
+			"Available tests are:\n\n"
+		);
+
+		for(t = 0; t < test_funcs_size; t++) {
+			const test_func_t* tf = &test_funcs[t];
+
+			printf("   %02d: %s (%s)\n", t + 1, tf->name, tf->descr);
+		}
+
+		printf("\nSequential by: Jeremy Moles (cubicool@gmail.com)\n");
+
+		return 1;
+	}
+
+	else if(argc == 2 && !strcmp(argv[1], "all")) run_tests(NULL, -1, SEQ_TRUE);
+
+	else {
+		int a;
+
+		for(a = 1; a < argc; a++) {
+			const char* str = argv[a];
+			seq_bool_t any = run_tests(str, atoi(str) - 1, SEQ_FALSE);
+
+			if(!any) printf("No tests matched your request '%s'.\n", str);
+		}
+	}
 
 	return 0;
 }
---
+++ test/seq-tmp.c
@@ -0,0 +1,6 @@
+#include <sequential.h>
+
+int main(int argc, char** argv) {
+	return 0;
+}
+
